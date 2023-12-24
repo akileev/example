f6f0520f262a34849dc8d4c2c38c3aa05c224549
Artem Kileev <AKileev@bellintegrator.com>
2023-12-24 16:32:18 +0500

    Tests

---
+++ src/test/java/org/example/ApplicationTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ApplicationTest {
+    @Test
+    public void exampleTest() {
+        assertThat(true).isTrue();
+    }
+}
