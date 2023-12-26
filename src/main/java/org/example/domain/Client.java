5516bf08aed52f3dd079ba8a22858aff4f0efa16
Artem Kileev <AKileev@bellintegrator.com>
2023-12-26 09:42:32 +0500

    Add database changelog

--- src/main/java/org/example/domain/Client.java
+++ src/main/java/org/example/domain/Client.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.UUID;
@@ -13,6 +15,7 @@ import java.util.UUID;
 @Table(name = "client")
 public class Client {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     @Column(name = "first_name")
--- src/test/java/org/example/service/ClientServiceTest.java
+++ src/test/java/org/example/service/ClientServiceTest.java
@@ -1,5 +1,7 @@
 package org.example.service;
 
+import org.example.model.ClientDto;
+import org.jeasy.random.EasyRandom;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,9 +14,19 @@ public class ClientServiceTest {
     @Autowired
     private ClientService service;
 
+    private final EasyRandom easyRandom = new EasyRandom();
+
     @Test
-    public void whenGetAllThenReturnCorrectValue() {
+    public void whenGetAllThenSuccess() {
         var actual = service.getAll(Pageable.ofSize(1));
         assertThat(actual).isEmpty();
+    }
+
+    @Test
+    public void whenCreateThenSuccess() {
+        var client = easyRandom.nextObject(ClientDto.class);
+        client.setId(null);
+        var actual = service.create(client);
+        assertThat(actual.getId()).isNotNull();
     }
 }
